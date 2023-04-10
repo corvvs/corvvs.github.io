99b404fb720408ad1d6de57c0e97f5ad3e6e6c0f
河田鷹介 <benefium@gmail.com>
2023-04-10 19:03:48 +0900

    conf?

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import getConfig from 'next/config';
 
+const conf = getConfig();
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  
+  const assetPrefix = conf.assetPrefix || "";
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -26,7 +28,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
+              src={`${assetPrefix}/vercel.svg`}
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -40,7 +42,7 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
+          src={`${assetPrefix}/next.svg`}
           alt="Next.js Logo"
           width={180}
           height={37}
