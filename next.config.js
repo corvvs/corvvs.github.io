30f575be11a2f001a0854d30863264ffe8e47493
河田鷹介 <benefium@gmail.com>
2023-04-10 13:35:49 +0900

    images.unoptimized = true

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === "production" ? "/life" : ""
+  assetPrefix: process.env.NODE_ENV === "production" ? "/life" : "",
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
