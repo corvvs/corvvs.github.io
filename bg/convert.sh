efe6190e7a3d3f0e953d03d8c2ba45c24f2c9187
corvvs <corvvs@users.noreply.github.com>
2023-04-21 04:30:11 +0000

    deploy: 2fd21f6343a0c6f89d8f85ceec508e8dd38decf9

--- bg/convert.sh
+++ bg/convert.sh
@@ -1,2 +1,3 @@
+ls large/bg*.jpg | xargs -I{} basename {} | xargs -I{} convert -resize "2048x1600>" large/{} {}
 ls bg*.jpg | xargs -I{} convert -resize 64x64^ -gravity center -crop 64x64+0+0 +repage {} thumb_{}
 ls bg*.jpg | xargs -I{} convert -resize 128x128 {} lofi_{}
