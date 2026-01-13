daa48e8a40ed4c148c8e546203896bb8c733eb3e
河田鷹介 <benefium@gmail.com>
2026-01-14 02:54:54 +0900

    ちょっと画像追加

--- states/config.ts
+++ states/config.ts
@@ -6,7 +6,8 @@ import _ from 'lodash';
 export const BackgroundImageNames = [
   "bg.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg", "bg5.jpg", "bg6.jpg", "bg7.jpg", 
   "bg8.jpg", "bg9.jpg", "bg10.jpg", "bg11.jpg", "bg12.jpg", "bg13.jpg", "bg14.jpg", "bg15.jpg",
-  "bg16.jpg", "bg17.jpg", "bg18.jpg", "bg19.jpg", "bg20.jpg", "bg21.jpg", "bg22.jpg",
+  "bg16.jpg", "bg17.jpg", "bg18.jpg", "bg19.jpg", "bg20.jpg", "bg21.jpg", "bg22.jpg", "bg23.jpg",
+  "bg24.jpg",
 ] as const;
 
 export type BackgroundImageName = typeof BackgroundImageNames[number];
