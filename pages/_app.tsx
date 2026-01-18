fca06a02eee8284575014d80b08ba8f81007d6ab
Yosuke Kawada <yosuke.kawada@algo-artis.com>
2026-01-18 11:26:46 +0900

    ちらつき防止

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,12 @@
 import '@/styles/globals.css'
-import Head from 'next/head'
 import type { AppProps } from 'next/app'
 import ShadowImageBox from '@/components/ShadowImageBox'
 import { GoogleAnalytics } from '../components/gtag'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-    <Head>
-      <style>{`
-        @font-face {
-          font-family: 'MaruMonica';
-          src: url("/fonts/x12y16pxMaruMonica.ttf");
-          font-weight: normal;
-        }
-      `}</style>
-      <style>{`
-        @font-face {
-          font-family: 'MaruMonica';
-          src: url("/fonts/x12y16pxMaruMonica.ttf");
-          font-weight: bold;
-        }
-      `}</style>
-
-    </Head>  
     <ShadowImageBox />
     <GoogleAnalytics />
     <Component {...pageProps} />
-    
   </>
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,11 +2,53 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="en" className="loading">
+      <Head>
+        {/* FOUC対策: 最優先で読み込まれるインラインスタイル */}
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+              html.loading { visibility: hidden; }
+              @font-face {
+                font-family: 'MaruMonica';
+                src: url("/fonts/x12y16pxMaruMonica.ttf");
+                font-weight: normal;
+                font-display: block;
+              }
+              @font-face {
+                font-family: 'MaruMonica';
+                src: url("/fonts/x12y16pxMaruMonica.ttf");
+                font-weight: bold;
+                font-display: block;
+              }
+            `,
+          }}
+        />
+        {/* フォントのプリロード */}
+        <link
+          rel="preload"
+          href="/fonts/x12y16pxMaruMonica.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              document.fonts.ready.then(function() {
+                document.documentElement.classList.remove('loading');
+              });
+              // 安全策: 最大3秒で強制表示
+              setTimeout(function() {
+                document.documentElement.classList.remove('loading');
+              }, 3000);
+            `,
+          }}
+        />
       </body>
     </Html>
   )
